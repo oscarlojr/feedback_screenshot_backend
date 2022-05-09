fb7cb347b1119584a116ab554741bbf35fec2a2c
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-05-09 16:16:58 -0300

    Método POST parametrizado

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,18 @@ import express from 'express'
 
 const app = express();
 
-app.get('/users',(req, res) => {
+app.use(express.json());
+
+//  GET, POST, PUT, PATCH, DELETE
+
+// GET =  Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+// PATCH = Atualizar uma informação única de uma entidade
+// DELETE = Deletar uma informação
+
+app.post('/feedbacks',(req, res) => {
+  console.log(req.body);
   return res.send('Hello Word');
 })
 
